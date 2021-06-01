36fb65299a0301480b92e406f0236455ace6ebb1
Clément <clement.savinaud@mines-ales.org>
2021-06-01 14:08:33 +0200

    Début de vérification pour l'ajout d'Address

--- address-app/address-service/src/main/java/org/sam/mines/address/service/impl/AddressServiceImpl.java
+++ address-app/address-service/src/main/java/org/sam/mines/address/service/impl/AddressServiceImpl.java
@@ -1,6 +1,7 @@
 package org.sam.mines.address.service.impl;
 
 import org.sam.mines.address.model.Address;
+import org.sam.mines.address.model.Town;
 import org.sam.mines.address.persistence.AddressRepository;
 import org.sam.mines.address.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +33,26 @@ public class AddressServiceImpl implements AddressService
     public Address save(Address address) {
         if (address.getNumber() == 0) {
             throw new IllegalArgumentException("Number is required");
+        } else if (address.getStreet().isEmpty()) {
+            throw new IllegalArgumentException("No addresses referenced for ");
+        } else if (!isTownValid(address.getTown())) {
+
+        } else if () {
+
         }
 
         return this.addressRepository.save(address);
+    }
+
+    private boolean isTownValid(Town town) throws IllegalArgumentException {
+        if (town.getPostCode() == 0) {
+            throw new IllegalArgumentException("Post code is required for each");
+        }
+
+        if (town.getName().isEmpty()) {
+            throw new IllegalArgumentException("Town name must be setted");
+        }
+        return true;
     }
 
     @Override
