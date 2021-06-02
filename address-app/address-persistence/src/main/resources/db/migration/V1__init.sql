b00c04efd0e729d4a31a6d121579c3bfb3cdaccf
dams <damien.carrier@mines-ales.org>
2021-06-02 12:12:34 +0200

    AJout de la table phone number dans la BDD.

--- address-app/address-persistence/src/main/resources/db/migration/V1__init.sql
+++ address-app/address-persistence/src/main/resources/db/migration/V1__init.sql
@@ -4,8 +4,11 @@ create table town(uuid uuid PRIMARY KEY, postCode NUMERIC, name text);
 
 create table address(uuid uuid PRIMARY KEY , number NUMERIC, street text, town_uuid uuid REFERENCES town(uuid));
 
+create table phonenumber(uuid uuid PRIMARY KEY, phoneNumber text);
+
 create table targetAddress (
   target_uuid uuid references target(uuid),
 
-  address_uuid uuid REFERENCES address(uuid), PRIMARY KEY(target_uuid, address_uuid));
+  address_uuid uuid REFERENCES address(uuid), PRIMARY KEY(target_uuid, address_uuid)
+);
 
