9e5d8bd43aab22febb8e5fd6b2aa24ef62644491
dams <damien.carrier@mines-ales.org>
2021-06-02 16:49:35 +0200

    modif de web context.

--- address-app/address-web/src/test/java/org/sam/mines/address/web/config/WebTestConfig.java
+++ address-app/address-web/src/test/java/org/sam/mines/address/web/config/WebTestConfig.java
@@ -1,7 +1,14 @@
 package org.sam.mines.address.web.config;
 
+import org.sam.mines.address.model.PhoneNumber;
+import org.sam.mines.address.persistence.AddressRepository;
+import org.sam.mines.address.persistence.PhoneNumberRepository;
+import org.sam.mines.address.persistence.TargetRepository;
 import org.sam.mines.address.persistence.TownRepository;
+import org.sam.mines.address.service.AddressService;
+import org.sam.mines.address.service.TargetService;
 import org.sam.mines.address.service.TownService;
+import org.sam.mines.address.web.controller.TownController;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +19,20 @@ public class WebTestConfig {
 
     @MockBean
     private TownRepository townRepository;
+
+    @MockBean
+    private TargetService targetService;
+
+    @MockBean
+    private TargetRepository targetRepository;
+
+    @MockBean
+    private AddressService addressService;
+
+    @MockBean
+    private AddressRepository addressRepository;
+
+    @MockBean
+    private PhoneNumberRepository phoneNumberRepository;
+
 }
