20e54447bc25ca754adb0a5a8154d1a8ac9e3dc5
Clément <clement.savinaud@mines-ales.org>
2021-06-04 11:22:59 +0200

    Mise en place des conteneurs + modification de la création de la BDD

--- address-app/address-persistence/src/main/resources/db/migration/V1__init.sql
+++ address-app/address-persistence/src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,5 @@
+create table phonenumber(uuid uuid PRIMARY KEY, phoneNumber text);
+
 create table target (
     uuid UUID PRIMARY KEY ,
     firstname text,
@@ -9,11 +11,9 @@ create table town(uuid uuid PRIMARY KEY, postCode NUMERIC, name text);
 
 create table address(uuid uuid PRIMARY KEY , number NUMERIC, street text, town_uuid uuid REFERENCES town(uuid));
 
-create table phonenumber(uuid uuid PRIMARY KEY, phoneNumber text);
 
 create table targetAddress (
   target_uuid uuid references target(uuid),
 
   address_uuid uuid REFERENCES address(uuid), PRIMARY KEY(target_uuid, address_uuid)
 );
-
